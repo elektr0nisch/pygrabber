94dbedc05645731f15d40c4a59e5f1e4e2e507e4
elektr0nisch <contact@elektronisch.dev>
2023-02-19 20:33:07 +0100

    Small changes

--- pygrabber.py
+++ pygrabber.py
@@ -11,8 +11,11 @@ session = aiohttp.ClientSession()
 async def index():
     if request.method == 'POST':
         url = request.form['url']
-        programme = await find_programme(field='website_url', target_value=url, options=MediathekOptions(working_directory="C:\\test", http_session=session))
+        programme = await find_programme(field='website_url', target_value=url, options=MediathekOptions(working_directory=".", http_session=session))
         if programme:
             return render_template('result.html', result=programme)
 
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
