d5f47a57df1a26754ecea6b62c9804f4e16abf17
elektr0nisch <contact@elektronisch.dev>
2023-02-21 19:26:31 +0100

    Add wsgi configuration

--- pygrabber.py
+++ pygrabber.py
@@ -5,17 +5,15 @@ import aiohttp
 
 app = Flask(__name__)
 
-session = aiohttp.ClientSession()
-
 @app.route('/', methods=('GET', 'POST'))
 async def index():
     if request.method == 'POST':
         url = request.form['url']
-        programme = await find_programme(field='website_url', target_value=url, options=MediathekOptions(working_directory=".", http_session=session))
+        programme = await find_programme(field='website_url', target_value=url, options=MediathekOptions(working_directory=".", http_session=aiohttp.ClientSession()))
         if programme:
             return render_template('result.html', result=programme)
 
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
